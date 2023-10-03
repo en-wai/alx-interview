5d8736206ade0f4099acb6ba39dd616674c8de14
en-wai <nyasare@hotmail.com>
2023-10-03 05:47:45 +0200

    Solve Error 10 in Pascals Triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -3,32 +3,33 @@
 0-pascal_triangle
 """
 
-def pascal_triangle(n):
+
+def generate_pascal_triangle(n):
     """
     Generate Pascal's Triangle up to the nth row.
 
     Args:
-    n (int): The number of rows to generate Pascal's Triangle.
+    n (int): The number of rows to generate Pascal's Triangle for.
 
     Returns:
     list of lists: A list of lists representing Pascal's Triangle up to the nth row.
                    Returns an empty list if n <= 0.
     """
-    result = []  # Initialize the result as an empty list
-
+    pascal_triangle = []
+    
     if n <= 0:
-        return result  # Return an empty list for n <= 0
-
-    result = [[1]]  # Initialize the result with the first row [1]
+        return pascal_triangle
+    
+    pascal_triangle = [[1]]
 
     for i in range(1, n):
-        temp_row = [1]  # Initialize a new row with the first element as 1
-
-        for j in range(len(result[i - 1]) - 1):
-            prev_row = result[i - 1]  # Get the previous row for calculations
-            temp_row.append(prev_row[j] + prev_row[j + 1])  # Calculate and append the next value
-
-        temp_row.append(1)  # Add the last element, which is also 1
-        result.append(temp_row)  # Append the new row to the result
-
-    return result
+        temp_row = [1]
+        prev_row = pascal_triangle[i - 1]
+        
+        for j in range(len(prev_row) - 1):
+            temp_row.append(prev_row[j] + prev_row[j + 1])
+        
+        temp_row.append(1)
+        pascal_triangle.append(temp_row)
+    
+    return pascal_triangle
