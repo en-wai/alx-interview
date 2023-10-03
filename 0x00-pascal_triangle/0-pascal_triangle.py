8e119efe8dbdcaaaf9542faa5157e41a6132ea56
en-wai <nyasare@hotmail.com>
2023-10-03 04:24:58 +0200

    Implement Pascals Triangle in Python

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -8,33 +8,27 @@ def pascal_triangle(n):
     Generate Pascal's Triangle up to the nth row.
 
     Args:
-    n (int): The number of rows to generate.
+    n (int): The number of rows to generate Pascal's Triangle.
 
     Returns:
     list of lists: A list of lists representing Pascal's Triangle up to the nth row.
                    Returns an empty list if n <= 0.
     """
+    result = []  # Initialize the result as an empty list
+
     if n <= 0:
-        return []  # Return an empty list for n <= 0
-    
-    # Initialize an empty list to store the triangle
-    triangle = []
-    
-    for i in range(n):
-        # Initialize a row with the first element as 1
-        row = [1]
-        
-        # Fill in the row with values based on the previous row
-        for j in range(1, i):
-            # Calculate the value by summing the two values above it
-            value = triangle[i - 1][j - 1] + triangle[i - 1][j]
-            row.append(value)
-        
-        # Add the last element, which is also 1
-        if i > 0:
-            row.append(1)
-        
-        # Append the row to the triangle
-        triangle.append(row)
-    
-    return triangle
+        return result  # Return an empty list for n <= 0
+
+    result = [[1]]  # Initialize the result with the first row [1]
+
+    for i in range(1, n):
+        temp_row = [1]  # Initialize a new row with the first element as 1
+
+        for j in range(len(result[i - 1]) - 1):
+            prev_row = result[i - 1]  # Get the previous row for calculations
+            temp_row.append(prev_row[j] + prev_row[j + 1])  # Calculate and append the next value
+
+        temp_row.append(1)  # Add the last element, which is also 1
+        result.append(temp_row)  # Append the new row to the result
+
+    return result
