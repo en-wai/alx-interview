6856541692978a8507ab6c6ba37cb1421a41606d
en-wai <nyasare@hotmail.com>
2023-10-08 19:34:45 +0200

    0x01 Lockboxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,33 +1,21 @@
 #!/usr/bin/python3
-"""
-This function determines if all boxes can be opened.
-It takes a list of lists, 'boxes', where each box may contain keys to other boxes.
-A key with the same number as a box opens that box.
-It returns True if all boxes can be opened, else it returns False.
-"""
 
 def canUnlockAll(boxes):
     """
-    Checks if all boxes can be unlocked.
-    
-    Args:
-        boxes (list of lists): A list of lists representing boxes and their keys.
-        
-    Returns:
-        bool: True if all boxes can be opened, False otherwise.
+    Function that checks with boolean value if the list type and
+    length to invoke two for iterations one to traverse the list
+    and the other to compaer if key is idx or not in order to open
     """
-    for key in range(1, len(boxes)):
-        key_found = False
-        for box_idx in range(len(boxes)):
-            if key in boxes[box_idx] and key != box_idx:
-                key_found = True
+    if type(boxes) is not list:
+        return False
+    elif (len(boxes)) == 0:
+        return False
+    for i in range(1, len(boxes) - 1):
+        boxes_checked = False
+        for index in range(len(boxes)):
+            boxes_checked = i in boxes[index] and i != index
+            if boxes_checked:
                 break
-        if not key_found:
-            return False
+        if boxes_checked is False:
+            return boxes_checked
     return True
-
-# Example usage:
-if __name__ == "__main__":
-    boxes = [[1], [2], [3], [0]]
-    print(canUnlockAll(boxes))  # Output: True
-
