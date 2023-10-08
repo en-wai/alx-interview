1488887b2f7080b7da404dd9cd31d7d00828cd78
en-wai <nyasare@hotmail.com>
2023-10-08 19:02:30 +0200

    0x01 Lockboxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,29 +1,20 @@
 #!/usr/bin/python3
+"""
+Method to determine if all boxes can be opened
+Using prototype: def canUnlockAll(boxes)
+"""
+
 
 def canUnlockAll(boxes):
-    # Create a set to keep track of visited boxes.
-    visited = set()
-    
-    # Initialize a stack for DFS and start with the first box (index 0).
-    stack = [0]
-    
-    # Iterate through the stack while there are boxes to visit.
-    while stack:
-        box_index = stack.pop()
-        
-        # Mark the current box as visited.
-        visited.add(box_index)
-        
-        # Iterate through the keys in the current box.
-        for key in boxes[box_index]:
-            # Check if the key opens a new box and if that box hasn't been visited yet.
-            if key < len(boxes) and key not in visited:
-                stack.append(key)
-    
-    # Check if all boxes have been visited.
-    return len(visited) == len(boxes)
-
-# Example usage:
-if __name__ == "__main__":
-    boxes = [[1], [2], [3], [0]]
-    print(canUnlockAll(boxes))  # Output: True
+    """
+    Check if boxes can be unlocked
+    """
+    for key in range(1, len(boxes) - 1):
+        ctr = False
+        for idx in range(len(boxes)):
+            ctr = (key in boxes[idx] and key != idx)
+            if ctr:
+                break
+        if ctr is False:
+            return ctr
+    return True
