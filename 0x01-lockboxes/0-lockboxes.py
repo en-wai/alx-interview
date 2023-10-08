0f0fe75443c3b8ca53b2094eaa912d1d5bcc0cfa
en-wai <nyasare@hotmail.com>
2023-10-08 19:09:29 +0200

    0x01 Lockboxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,20 +1,33 @@
 #!/usr/bin/python3
 """
-Method to determine if all boxes can be opened
-Using prototype: def canUnlockAll(boxes)
+This function determines if all boxes can be opened.
+It takes a list of lists, 'boxes', where each box may contain keys to other boxes.
+A key with the same number as a box opens that box.
+It returns True if all boxes can be opened, else it returns False.
 """
-
 
 def canUnlockAll(boxes):
     """
-    Check if boxes can be unlocked
+    Checks if all boxes can be unlocked.
+    
+    Args:
+        boxes (list of lists): A list of lists representing boxes and their keys.
+        
+    Returns:
+        bool: True if all boxes can be opened, False otherwise.
     """
-    for key in range(1, len(boxes) - 1):
-        ctr = False
-        for idx in range(len(boxes)):
-            ctr = (key in boxes[idx] and key != idx)
-            if ctr:
+    for key in range(1, len(boxes)):
+        key_found = False
+        for box_idx in range(len(boxes)):
+            if key in boxes[box_idx] and key != box_idx:
+                key_found = True
                 break
-        if ctr is False:
-            return ctr
+        if not key_found:
+            return False
     return True
+
+# Example usage:
+if __name__ == "__main__":
+    boxes = [[1], [2], [3], [0]]
+    print(canUnlockAll(boxes))  # Output: True
+
