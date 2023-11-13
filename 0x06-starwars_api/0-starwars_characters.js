f965e2448d5bedc6740b84395f39e43ea2d01da0
en-wai <nyasare@hotmail.com>
2023-11-13 00:13:02 +0000

    Fix error check 7 in start wars API

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,42 +2,30 @@
 
 const request = require('request');
 
-// Retrieve the film ID from the command line arguments
-const filmId = process.argv[2];
+const movieId = process.argv[2];
+const movieEndpoint = 'https://swapi-api.alx-tools.com/api/films/' + movieId;
 
-// Construct the API endpoint for fetching film information
-const filmEndpoint = 'https://swapi-api.alx-tools.com/api/films/' + filmId;
-
-// Function to fetch and display character names
-function fetchData(characterUrls, index) {
-  // Base case: check if all characters have been processed
-  if (characterUrls.length === index) {
+function sendRequest (characterList, index) {
+  if (characterList.length === index) {
     return;
   }
 
-  // Make a request to the API endpoint for a specific character
-  request(characterUrls[index], (error, response, body) => {
+  request(characterList[index], (error, response, body) => {
     if (error) {
       console.log(error);
     } else {
-      // Display the name of the character
       console.log(JSON.parse(body).name);
-      
-      // Recursively fetch and display the next character
-      fetchData(characterUrls, index + 1);
+      sendRequest(characterList, index + 1);
     }
   });
 }
 
-// Make a request to the API endpoint for film information
-request(filmEndpoint, (error, response, body) => {
+request(movieEndpoint, (error, response, body) => {
   if (error) {
     console.log(error);
   } else {
-    // Extract character URLs from the film information
-    const characterUrls = JSON.parse(body).characters;
-    
-    // Initiate the process of fetching and displaying character names
-    fetchData(characterUrls, 0);
+    const characterList = JSON.parse(body).characters;
+
+    sendRequest(characterList, 0);
   }
 });
