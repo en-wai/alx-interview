2dc195b91ae1509dcd266d59824f69828136ca7e
en-wai <nyasare@hotmail.com>
2023-11-21 10:31:45 +0000

    Solve min operations project

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,32 +1,21 @@
 #!/usr/bin/python3
-"""Minimum Operations to Generate 'n' Characters of 'H'
-"""
-
-
-def min_operations_to_generate_n_characters(n: int) -> int:
-    """Calculate the minimum operations needed to generate 'n' H characters.
-
-    Args:
-        n (int): The desired number of H characters.
-
-    Returns:
-        int: The minimum operations required.
-
+""" Minimum Operations
     """
-    current_sequence = 'H'
-    base_sequence = 'H'
-    operations_count = 0
 
-    while len(current_sequence) < n:
-        if n % len(current_sequence) == 0:
-            operations_count += 2
-            base_sequence = current_sequence
-            current_sequence += current_sequence
+
+def minOperations(n: int) -> int:
+    """ Minimum Operations needed to get n H characters """
+    next = 'H'
+    body = 'H'
+    op = 0
+    while (len(body) < n):
+        if n % len(body) == 0:
+            op += 2
+            next = body
+            body += body
         else:
-            operations_count += 1
-            current_sequence += base_sequence
-
-    if len(current_sequence) != n:
+            op += 1
+            body += next
+    if len(body) != n:
         return 0
-
-    return operations_count
+    return op
